117f15710c2407605c2eba23ebc34a355ade8f71
linda <lindatochukwu30@gmail.com>
2020-09-13 22:13:11 +0100

    trying to deploy

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ app.post("/designs/:id/comments", (req, res) => {
 });
 
 
-const port = 3000;
+const port =  process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Port ${port} is listening..... `)
 });
